6dfee86ab479d1eb504b442316fca6804068ef6c
Damián <damian.sciutto@zoomagri.com>
2020-09-18 01:24:51 -0300

    Comment model

--- src/models/MateriaModel.ts
+++ src/models/MateriaModel.ts
@@ -3,13 +3,13 @@ import Sequelize from 'sequelize';
 export default function MateriaSchema(sequelize: Sequelize.Sequelize) {
     const model = sequelize.define('materias', {
         nombre: Sequelize.STRING(45),
-        turno_id: {
-            type: Sequelize.INTEGER,
-            references: {
-                model: 'turnos',
-                key: 'id'
-            }
-        },
+        // turno_id: {
+        //     type: Sequelize.INTEGER,
+        //     references: {
+        //         model: 'turnos',
+        //         key: 'id'
+        //     }
+        // },
         estado: Sequelize.ENUM('activo', 'baja'),
         duracion: Sequelize.ENUM('cuatrimestral', 'anual')
     },{
