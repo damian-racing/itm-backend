88c137954eca34a2b33d07c7a0db8cc677fe7b59
Damián <damian.sciutto@zoomagri.com>
2020-09-22 17:07:11 -0300

    Validación de carreras con materias

--- src/controllers/CarreraMateriaController.ts
+++ src/controllers/CarreraMateriaController.ts
@@ -7,7 +7,16 @@ export default class CarreraMateriaController {
     
     public async create(req: express.Request, res: express.Response) {
         const collection = req.body;
-        
+        if (Object.entries(collection).length === 0 || collection.length === 0) return res.status(400).send(errorResponse(400, Error("Por favor seleccione la carrera con las materias")));
+
+        const query = {
+            where: {
+                carrera_id: collection.map((element: any) => element.carrera_id).shift()
+            }
+        }
+        CarreraMateriaModel.destroy(query)
+        .catch((error: Error) => res.status(500).send(errorResponse(500, error)))
+
         CarreraMateriaModel.bulkCreate(collection, {ignoreDuplicates: true})
         .then(collection => res.status(201).json(successResponse(collection)))
         .catch((error: Error) => res.status(500).send(errorResponse(500, error)));
--- src/models/CarreraMateriaModel.ts
+++ src/models/CarreraMateriaModel.ts
@@ -4,8 +4,8 @@ import Sequelize, { Model } from 'sequelize';
 export default class CarreraMateriaModel extends Model {}
 
 CarreraMateriaModel.init({
-    carrera_id: { type: Sequelize.INTEGER,  unique: 'compositeIndex' },
-    materia_id: { type: Sequelize.INTEGER,  unique: 'compositeIndex' },
+    carrera_id: { type: Sequelize.INTEGER },
+    materia_id: { type: Sequelize.INTEGER },
 }, { sequelize, modelName: 'carreras_materias', timestamps: true });
 
 CarreraMateriaModel.removeAttribute('id');
